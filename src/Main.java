6563a6538f3805dd14bad39024828f974a96b32f
jeffmartinedvan <jeffmartinedvan@gmail.com>
2024-03-20 13:48:53 +0700

    feat(main): Making duplicate of system out println

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
         System.out.println("This is my first code");
     }
 }
